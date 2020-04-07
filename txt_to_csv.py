5e3e16d0148b8ddf746ab9057771ce5e2f4c943b
Duong Dang <40470500+duongdqq@users.noreply.github.com>
2020-04-07 17:53:11 +0700

    Add files via upload

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
-read_file = pd.read_csv(r'/media/veec20/Data/duongdq/darknet/combine_txt.txt')
-read_file.to_csv('/media/veec20/Data/duongdq/darknet/change_txt_csv.csv', index=None)
+filename = 'statistic_data.txt'
+
+df = pd.read_csv(filename, header=None, error_bad_lines=False, encoding='utf8')
+
+df.columns = ['all']
+
+coordination = df['all'].str.split(' ', n=5, expand=True)
+
+df['id_class'] = coordination[0]
+df['x_center'] = coordination[1]
+df['y_center'] = coordination[2]
+df['width'] = coordination[3]
+df['height'] = coordination[4]
+
+df = df.drop(columns=['all'])
+
+df.to_csv('converted_csv.csv', index=False)
